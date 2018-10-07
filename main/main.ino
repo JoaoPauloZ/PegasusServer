300bc2d6e175ba8cf320350b2f60b158de949b86
João Paulo Serodio <serodio@premiersoft.net>
2018-10-07 15:18:05 -0300

    Still working.

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,8 @@
 #include "Arduino.h"
 #include "PegasusServer.h"
-#include "PegasusProcessor.h"
-#include "PegasusFlightController.h"
 
 //https://www.arduinoecia.com.br/2015/07/como-ligar-motor-brushless-arduino.html
 PegasusServer server;
-PegasusProcessor processor;
-PegasusFlightController flightController;
-
-String buffer = "";
 
 void setup() {
    Serial.begin(9600);
@@ -19,7 +13,6 @@ void setup() {
  
    // while server is not conected wait
    while (!server.isConnected) {}
-
 }
 
 void loop() {
